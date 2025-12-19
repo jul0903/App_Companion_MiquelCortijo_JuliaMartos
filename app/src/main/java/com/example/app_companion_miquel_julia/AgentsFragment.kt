4e617d5b0229e23584c16878a12de501ee34d7dc
Júlia <julia.martos@enti.cat>
2025-12-19 18:39:09 +0100

    Bottom navbar done

---
+++ app/src/main/java/com/example/app_companion_miquel_julia/AgentsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.app_companion_miquel_julia
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AgentsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_agents, container, false)
+    }
+}
--- app/src/main/java/com/example/app_companion_miquel_julia/MainActivity.kt
+++ app/src/main/java/com/example/app_companion_miquel_julia/MainActivity.kt
@@ -2,11 +2,76 @@ package com.example.app_companion_miquel_julia
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var myNavBar: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        myNavBar = findViewById(R.id.navbar)
+
+        myNavBar.setOnItemSelectedListener { item -> handleNavigationItemSelected(item.itemId) }
+
+        if(supportFragmentManager.findFragmentById(R.id.fragment) == null){
+            loadFragment(NewsFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fragment, fragment).commit()
+    }
+
+    private fun handleNavigationItemSelected(itemId: Int): Boolean {
+        return when (itemId){
+            R.id.news -> {
+                loadFragment(NewsFragment())
+                true
+            }
+            R.id.agents -> {
+                loadFragment(AgentsFragment())
+                true
+            }
+            R.id.maps -> {
+                loadFragment(MapsFragment())
+                true
+            }
+            R.id.settings -> {
+                loadFragment(SettingsFragment())
+                true
+            }
+            else -> false
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
